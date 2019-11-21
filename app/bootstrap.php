dbef411f9d3a8ecf98ddf5869fe3159ffa2b4179
Steven Marshall <steven@nevvix.com>
2019-11-20 20:10:21 -0800

    Moved bootstrap.php to app/

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-define('ROOT', __DIR__);
+define('ROOT', dirname(__DIR__));
 define('APP', ROOT.'/app');
 define('WWW', ROOT.'/www');
 
--- app/html/index.php
+++ app/html/index.php
@@ -1,4 +1,4 @@
 <?php
 define('DIR', __DIR__);
-require_once substr_replace(__FILE__, 'bootstrap.php', strpos(__FILE__, 'www'));
+require_once substr_replace(__FILE__, 'app/bootstrap.php', strpos(__FILE__, 'www'));
 PWF::load('html.php');
